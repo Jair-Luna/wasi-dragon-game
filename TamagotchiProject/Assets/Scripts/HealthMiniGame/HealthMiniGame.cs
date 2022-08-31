489886d348f52e5a6bee048f38d31af831032253
Jair-Luna <lunajair90@gmail.com>
2022-08-31 16:21:56 -0500

    F: Adding minigames

---
+++ TamagotchiProject/Assets/Scripts/HealthMiniGame/HealthMiniGame.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UI;
+using UnityEngine;
+
+
+namespace HealthMiniGame
+{
+    public class HealthMiniGame : MonoBehaviour
+    {
+        [SerializeField] private GameObject ghostSpawnerPrefab;
+        [SerializeField] private GameObject pieMenuGameObject;
+
+        private PieMenu pieMenu;
+        private GameObject[] eyes;
+
+        private void Start()
+        {
+            pieMenu = pieMenuGameObject.GetComponent<PieMenu>();
+            eyes = GameObject.FindGameObjectsWithTag("Eye");
+        }
+
+        public void StartEnergyMiniGame()
+        {
+            Hearth.GhostCounter = 5;
+            StartCoroutine(CloseEyes());
+            Instantiate(ghostSpawnerPrefab, transform);
+        }
+
+        public void StopEnergyMiniGame()
+        {
+            pieMenu.DisableActionMode();
+            OpenEyes();
+        }
+
+        private void OpenEyes()
+        {
+            foreach(var eye in eyes)
+            {
+                eye.SetActive(true);
+            }
+        }
+
+        private IEnumerator CloseEyes()
+        {
+            yield return new WaitForSeconds(0.7f);
+            foreach(var eye in eyes)
+            {
+                eye.SetActive(false);
+            }
+        }
+    }
+}
+
